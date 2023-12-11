8a49a593d4966f84f0ed0d7fef35688cdd982fbb
Dmitriy <martcarrefour@gmail.com>
2023-12-11 23:55:32 +0300

    fix UpdateTagDto

--- src/tag/dto/update-tag.dto.ts
+++ src/tag/dto/update-tag.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTagDto } from './create-tag.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateTagDto extends PartialType(CreateTagDto) {}
+export class UpdateTagDto extends PartialType(CreateTagDto) {
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    title: string;
+}
